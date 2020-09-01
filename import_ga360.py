39a7a9e2681e347d96445d750eb1fb8aacaca3d7
ldaugusto <ldaugusto@gmail.com>
2020-09-01 16:31:24 +0100

    adding gcs flow

--- import_ga360.py
+++ import_ga360.py
@@ -5,6 +5,7 @@ from airflow.contrib.operators import bigquery_get_data
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 from airflow.contrib.operators.bigquery_to_gcs import BigQueryToCloudStorageOperator
 from airflow.contrib.operators.bigquery_table_delete_operator import BigQueryTableDeleteOperator
+from airflow.providers.amazon.aws.transfers.gcs_to_s3 import GCSToS3Operator
 
 from airflow.operators import bash_operator
 from airflow.operators import email_operator
@@ -33,27 +34,40 @@ dag = DAG('ga360_etl',
         	schedule_interval=timedelta(days=1),
         	default_args=default_args)
 
-extract_ga360 = BigQueryOperator(
+prepare_ga360 = BigQueryOperator(
 	dag=dag,
-    task_id='bq_unnest_yesterdays_table',
+    task_id='bq_unnest_table',
     bigquery_conn_id='zwift_ga360_bigquery',
     sql="""SELECT visitId, clientId FROM `{source_table}`""".format(source_table=source_table),
     use_legacy_sql=False,
-    destination_dataset_table=target_table)
+    destination_dataset_table=target_table
+    )
 
-copy_ga360_to_gcs = BigQueryToCloudStorageOperator(
+extract_ga360_to_gcs = BigQueryToCloudStorageOperator(
 	dag=dag,
     task_id='export_recent_yesterday_to_gcs',
     bigquery_conn_id='zwift_ga360_bigquery',
     source_project_dataset_table=target_table,
     destination_cloud_storage_uris=[output_file],
     compression='GZIP',
-    export_format='CSV')
+    export_format='CSV'
+    )
+
+export_gcs_to_s3 = GCSToS3Operator(
+	dag=dag,
+    task_id="cp_gcs_to_s3", 
+    gcp_conn_id='zwift_ga360_bigquery',
+    dest_aws_conn_id='local_s3',
+    dest_verify=False,
+    bucket=gcs_bucket
+)
 
 delete_tmp_table = BigQueryTableDeleteOperator(
 	dag=dag,
     task_id="delete_tmp_table", 
     bigquery_conn_id='zwift_ga360_bigquery',
-    deletion_dataset_table=target_table)
+    deletion_dataset_table=target_table
+    )
 
-extract_ga360 >> copy_ga360_to_gcs >> delete_tmp_table
+
+prepare_ga360 >> extract_ga360_to_gcs >> export_gcs_to_s3 >> delete_tmp_table
